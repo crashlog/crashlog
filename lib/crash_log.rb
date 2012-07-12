b72fcc8e6481e17eb844cfd6c0a3f555fe0b69e7
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-13 09:32:18 +1000

    Updated CrashLog class interface

--- lib/crash_log.rb
+++ lib/crash_log.rb
@@ -1,5 +1,23 @@
 require 'crash_log/version'
+begin
+  require 'active_support'
+  require 'active_support/core_ext'
+rescue LoadError
+  require 'activesupport'
+  require 'activesupport/core_ext'
+end
+require 'faraday'
+require 'crash_log/railtie' if defined?(Rails::Railtie)
+require 'crash_log/logging'
 
 module CrashLog
+  LOG_PREFIX = "** [CrashLog] "
+  extend Logging
 
+  class << self
+
+    def report_for_duty!
+      info("CrashLog initialized and ready to handle exceptions")
+    end
+  end
 end
