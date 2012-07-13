bb1b421a42e2650f357bfd82860ab92b2ed01822
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-13 16:57:04 +1000

    Added examples for testing CrashLog class API

--- spec/crash_log_spec.rb
+++ spec/crash_log_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 
 describe CrashLog do
+  let(:raised_error) do
+    begin
+      raise RuntimeError, "This broke"
+    rescue RuntimeError => e
+      e
+    end
+  end
+
   describe '.notify' do
-    it 'handles being passed an exception object'
+    it 'does not send if not live'
+    it 'handles being passed an exception object' do
+      CrashLog::Reporter.any_instance.should_receive(:notify).once
+
+      CrashLog.stub(:live?).and_return(true)
+      CrashLog.notify(raised_error)
+      # CrashLog::Reporter.new.notify({})
+    end
+
     it 'handles being passed an exception object and user data'
     it 'handles being passed a string'
   end
@@ -13,14 +29,73 @@ describe CrashLog do
   end
 
   describe '.configuration' do
-    it 'handles being configured with a block'
-    it 'handles directly configuring attributes'
+    after do
+      CrashLog.instance_variable_set("@configuration", nil)
+    end
+
+    it 'handles being configured with a block' do
+      logger = stub("Logger")
+      logger.stub(:error)
+
+      CrashLog.configuration.logger.should be_nil
+      CrashLog.configure do |config|
+        config.logger = logger
+      end
+      CrashLog.configuration.logger.should == logger
+    end
+
+    it 'handles directly configuring attributes' do
+      logger = stub("Logger")
+      CrashLog.configuration.logger.should be_nil
+      CrashLog.configuration.logger = logger
+      CrashLog.configuration.logger.should == logger
+    end
+
+    it 'accepts api_key' do
+      key = stub("THIS IS AN API KEY")
+      CrashLog.configuration.api_key.should be_nil
+      CrashLog.configuration.api_key = key
+      CrashLog.configuration.api_key.should == key
+    end
   end
 
   describe '.ready' do
     it 'logs an ready message' do
-      CrashLog.should_receive(:info).with("CrashLog initialized and ready to handle exceptions")
+      logger = stub('Logger')
+      logger.should_receive(:info).with("** [CrashLog] Initialized and ready to handle exceptions")
+
+      CrashLog.stub(:logger).and_return(logger)
       CrashLog.report_for_duty!
+    end
+  end
+
+  describe '.live?' do
+    it 'is live if current stage is included in release stages' do
+      CrashLog.configure do |c|
+        c.release_stages = ['test']
+        c.stage = 'test'
+      end
+
+      CrashLog.should be_live
+    end
+
+    it 'is not live if current stage is not included in release stages' do
+      CrashLog.configure do |c|
+        c.release_stages = ['production']
+        c.stage = 'test'
+      end
+
+      CrashLog.should_not be_live
+    end
+
+    it 'handles irregular stage names' do
+      CrashLog.configure do |c|
+        c.release_stages = ['test']
+        c.stage = 'Test'
+      end
+
+      CrashLog.configuration.stage.should === 'test'
+      CrashLog.should be_live
     end
   end
 end
