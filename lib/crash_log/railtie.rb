a32ec283b882bf53c23a3d1f0a2d09c81619675e
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-13 16:55:49 +1000

    Changed environment.name to :stage

--- lib/crash_log/railtie.rb
+++ lib/crash_log/railtie.rb
@@ -7,7 +7,7 @@ module CrashLog
     config.after_initialize do
       CrashLog.configure do |config|
         config.logger           ||= ::Rails.logger
-        config.environment_name ||= ::Rails.env
+        config.stage            ||= ::Rails.env
         config.project_root     ||= ::Rails.root
         config.framework        = "Rails: #{::Rails::VERSION::STRING}"
       end
