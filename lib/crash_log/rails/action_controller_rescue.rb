902810a5892dc7bb6829617d7f625b79a1b79342
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-14 14:16:04 +1000

    Added controller methods - needs tests

---
+++ lib/crash_log/rails/action_controller_rescue.rb
@@ -0,0 +1,32 @@
+module CrashLog
+  module Rails
+
+    # CrashLog controller integration
+    # Aliases method chain for Rails internal rescue action
+    module ActionControllerRescue
+      def self.included(base)
+        base.send(:alias_method, :rescue_action_in_public_without_crash_log, :rescue_action_in_public)
+        base.send(:alias_method, :rescue_action_in_public, :rescue_action_in_public_with_crash_log)
+
+        base.send(:alias_method, :rescue_action_locally_without_crash_log, :rescue_action_locally)
+        base.send(:alias_method, :rescue_action_locally, :rescue_action_locally_with_crash_log)
+      end
+
+    private
+
+      # crash_log_context is defined in controller_methods.rb
+      def rescue_action_in_public_with_crash_log(exception)
+        crash_log.auto_notify(exception, crash_log_context)
+        rescue_action_in_public_without_crash_log(exception)
+      end
+
+      # crash_log_context is defined in controller_methods.rb
+      def rescue_action_locally_with_crash_log(exception)
+        crash_log.auto_notify(exception, crash_log_context)
+        rescue_action_locally_without_crash_log(exception)
+      end
+    end
+  end
+
+end
+
---
+++ lib/crash_log/rails/controller_methods.rb
@@ -0,0 +1,44 @@
+module CrashLog
+  module Rails
+    module ControllerMethods
+
+      def crash_log_context
+        { :parameters       => crash_log_filter_if_filtering(params.to_hash),
+          :session_data     => crash_log_filter_if_filtering(crash_log_session_data),
+          :controller       => params[:controller],
+          :action           => params[:action],
+          :url              => crash_log_request_url,
+          :cgi_data         => crash_log_filter_if_filtering(request.env)
+        }
+      end
+
+
+    private
+      def notify_crashlog(exception, custom_data = nil)
+        request_data = crash_log_context
+        request_data[:meta_data][:custom] = custom_data if custom_data
+        CrashLog.notify(exception, request_data)
+      end
+
+      def crash_log_session_data
+        if session.respond_to?(:to_hash)
+          session.to_hash
+        else
+          session.data
+        end
+      end
+
+      def crash_log_request_url
+        url = "#{request.protocol}#{request.host}"
+
+        unless [80, 443].include?(request.port)
+          url << ":#{request.port}"
+        end
+
+        url << request.fullpath
+        url
+      end
+
+    end
+  end
+end
