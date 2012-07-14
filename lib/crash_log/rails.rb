b43d23c4be42c7d28c5dfab227d14fc0b5ff4900
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-14 14:16:24 +1000

    Added Rails 2 initializer

--- lib/crash_log/rails.rb
+++ lib/crash_log/rails.rb
@@ -1,33 +1,32 @@
-# Rails 2.x support
+require "crash_log"
+require "crash_log/rails/controller_methods"
+require "crash_log/rails/action_controller_rescue"
 
-# require "bugsnag"
-# require "bugsnag/rails/controller_methods"
-# require "bugsnag/rails/action_controller_rescue"
-# 
-# module Bugsnag
-#   module Rails
-#     def self.initialize
-#       if defined?(ActionController::Base)
-#         ActionController::Base.send(:include, Bugsnag::Rails::ActionControllerRescue)
-#         ActionController::Base.send(:include, Bugsnag::Rails::ControllerMethods)
-#       end
-# 
-#       # Try to find where to log to
-#       rails_logger = nil
-#       if defined?(::Rails.logger)
-#         rails_logger = ::Rails.logger
-#       elsif defined?(RAILS_DEFAULT_LOGGER)
-#         rails_logger = RAILS_DEFAULT_LOGGER
-#       end
-# 
-#       Bugsnag.configure do |config|
-#         config.logger = rails_logger
-#         config.release_stage = RAILS_ENV  if defined?(RAILS_ENV)
-#         config.project_root = RAILS_ROOT if defined?(RAILS_ROOT)
-#         config.framework = "Rails: #{::Rails::VERSION::STRING}" if defined?(::Rails::VERSION)
-#       end
-#     end
-#   end
-# end
-# 
-# Bugsnag::Rails.initialize
+# Rails 2.x support
+module CrashLog
+  module Rails
+    def self.initialize
+      if defined?(ActionController::Base)
+        ActionController::Base.send(:include, CrashLog::Rails::ActionControllerRescue)
+        ActionController::Base.send(:include, CrashLog::Rails::ControllerMethods)
+      end
+
+      # Try to find where to log to
+      rails_logger = nil
+      if defined?(::Rails.logger)
+        rails_logger = ::Rails.logger
+      elsif defined?(RAILS_DEFAULT_LOGGER)
+        rails_logger = RAILS_DEFAULT_LOGGER
+      end
+
+      CrashLog.configure do |config|
+        config.logger = rails_logger
+        config.release_stage = RAILS_ENV  if defined?(RAILS_ENV)
+        config.project_root = RAILS_ROOT if defined?(RAILS_ROOT)
+        config.framework = "Rails: #{::Rails::VERSION::STRING}" if defined?(::Rails::VERSION)
+      end
+    end
+  end
+end
+
+CrashLog::Rails.initialize
