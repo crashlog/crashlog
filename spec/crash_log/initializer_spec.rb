15fc5fd7320ff3322a1c54eeeaf3d64a5a6beddb
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-14 14:15:32 +1000

    Added initialiser spec for Rails 2

---
+++ spec/crash_log/initializer_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "Initializer" do
+
+  let(:logger) { stub("Logger") }
+  let(:other_logger) { stub("OtherLogger") }
+
+  describe 'auto configure logger' do
+    before do
+      unless defined?(Rails)
+        module Rails
+        end
+      end
+      Rails.stub(:logger).and_return(logger)
+      logger.stub(:error)
+      other_logger.stub(:error)
+    end
+
+
+    it 'detects presence of Rails logger' do
+      CrashLog::Rails.initialize
+      CrashLog.logger.should be(logger)
+    end
+
+    it "allows overriding of the logger if already assigned" do
+      unless defined?(Rails)
+        module Rails
+        end
+      end
+
+      Rails.stub(:logger).and_return(logger)
+
+      CrashLog::Rails.initialize
+      CrashLog.logger.should == logger
+
+      CrashLog.configure do |config|
+        config.logger = other_logger
+      end
+
+      CrashLog.logger.should == other_logger
+    end
+  end
+
+end
