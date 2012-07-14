d9c1ea863cd9e7196bc4781948a9ea4e7b4160e9
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-14 14:15:53 +1000

    Included controller methods into Railstie

--- lib/crash_log/railtie.rb
+++ lib/crash_log/railtie.rb
@@ -1,6 +1,7 @@
 require 'crash_log'
 require 'rails'
 
+# Rails 3.x support
 module CrashLog
   class Railtie < ::Rails::Railtie
 
@@ -11,6 +12,15 @@ module CrashLog
         config.project_root     ||= ::Rails.root
         config.framework        = "Rails: #{::Rails::VERSION::STRING}"
       end
+
+      # Attach our Rails Controller methods
+      ActiveSupport.on_load(:action_controller) do
+        # Lazily load action_controller methods
+        require 'crash_log/rails/controller_methods'
+
+        include CrashLog::Rails::ControllerMethods
+      end
+
     end
 
   end
