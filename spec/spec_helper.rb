d48fb4357b9d8edf544884f9ea6b517743bb369e
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-07-28 17:02:10 +1000

    Updated spec helper to load dummy app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,14 @@
+if ENV['COV']
+  require 'simplecov'
+  SimpleCov.start
+end
+
+require 'delorean'
+require 'json_spec'
+require 'uuid'
 require "crash_log"
+
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 Dir[File.expand_path("../support/*.rb", __FILE__)].each { |file| require file }
 
