2ae888f98e67463567b03e86ece41230dd16fff4
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-09-06 21:28:59 +1000

    Updated reporter spec

--- spec/crash_log/reporter_spec.rb
+++ spec/crash_log/reporter_spec.rb
@@ -3,27 +3,36 @@ require 'spec_helper'
 describe CrashLog::Reporter do
   let(:uuid) { UUID.generate }
 
-  let(:config) { stub("Configuration", {
-    :host => "io.crashlog.io",
-    :scheme => "https",
-    :port => 443,
-    :endpoint => '/notify',
-    :announce => true,
-    :announce_endpoint => '/announce',
-    :dry_run => false,
-    :api_key => 'API_TOKEN',
-    :project_id => "PROJECT_ID",
-    :http_read_timeout => 1,
-    :http_open_timeout => 1
-    }).tap do |config|
-      config.stub(:adapter).and_return(:test, &test_adapter)
+  let(:config) {
+    CrashLog::Configuration.new.tap do |config|
+      config.project_id = 'PROJECT_ID'
+      config.api_key = 'API_KEY'
+      # config.adapter = test_adapter
+      config.scheme = 'http'
     end
   }
 
+  # let(:config) { stub("Configuration", {
+  #   :host => "io.crashlog.io",
+  #   :scheme => "https",
+  #   :port => 443,
+  #   :endpoint => '/notify',
+  #   :announce => true,
+  #   :announce_endpoint => '/announce',
+  #   :dry_run => false,
+  #   :api_key => 'API_TOKEN',
+  #   :project_id => "PROJECT_ID",
+  #   :http_read_timeout => 1,
+  #   :http_open_timeout => 1
+  #   }).tap do |config|
+  #     config.stub(:adapter).and_return(:test, &test_adapter)
+  #   end
+  # }
+
   let(:test_adapter) {
     lambda { |stub|
-      stub.post('/notify') do |env|
-        [200, {}, env[:request_headers]]
+      stub.post('/events') do |env|
+        [201, {}, env[:request_headers]]
       end
     }
   }
@@ -49,27 +58,47 @@ describe CrashLog::Reporter do
     CrashLog.stub(:report_for_duty!)
   end
 
+  it 'should not be doing a dry run' do
+    subject.should_not be_dry_run
+  end
+
   describe '#notify' do
     before do
       test_connection = Faraday.new(:url => subject.url) do |builder|
         builder.adapter :test, stubs
+        builder.request :hmac_authentication, 'PROJECT_ID', 'SECRET', {:service_id => 'CrashLog'}
+        builder.request :url_encoded
       end
 
       subject.stub(:connection).and_return(test_connection)
     end
 
-
     let!(:stubs) do
       Faraday::Adapter::Test::Stubs.new do |stub|
-        stub.post('/notify') { [200, {:token => "TOKEN"}, positive_response_json] }
+        stub.post('/events') { [200, {}, positive_response_json] }
       end
     end
 
     after do
+      #stubs.verify_stubbed_calls
+    end
+
+    it 'makes a post request' do
+      subject.send(:connection).should_receive(:post).once
+      subject.notify(payload)
+    end
+
+    it 'authenticates request with HMAC' do
+      subject.notify(payload).should be_true
+      subject.response.env[:request_headers]['Authorization'].should ==
+        CrashLog::AuthHMAC.new({}, {:service_id => 'CrashLog'}).authorization(subject.response.env, 'PROJECT_ID', 'SECRET')
       stubs.verify_stubbed_calls
     end
+
     it 'sends a serialized payload to crashlog.io' do
       subject.notify(payload).should be_true
+
+      stubs.verify_stubbed_calls
     end
 
     it 'captures result body' do
@@ -81,7 +110,7 @@ describe CrashLog::Reporter do
   describe '#announce' do
     let!(:stubs) do
       Faraday::Adapter::Test::Stubs.new do |stub|
-        stub.post('/announce') { [200, {}, announce_json] }
+        stub.post('/announce') { [201, {}, announce_json] }
       end
     end
 
@@ -103,10 +132,9 @@ describe CrashLog::Reporter do
 
   describe 'url' do
     it 'constructs url from configuration' do
-      subject.url.to_s.should == 'https://io.crashlog.io'
+      subject.url.to_s.should == 'http://stdin.crashlog.io'
     end
 
-    it 'sends auth_token as request parameter'
     it 'sends HMACAuth header'
 
     #   # config.stub(:adapter).and_return(:test, &test_adapter)
