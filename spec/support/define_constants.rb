a007b441ae72912f1fa068a8aaa37a6ba28da757
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-10-19 10:03:16 +1100

    Enhanced define constants

--- spec/support/define_constants.rb
+++ spec/support/define_constants.rb
@@ -1,15 +1,26 @@
 module DefinesConstants
   def setup_constants
     @defined_constants = []
+    @old_constants = []
   end
 
   def teardown_constants
     @defined_constants.each do |constant|
       Object.__send__(:remove_const, constant)
+      @old_constants.each do |old|
+        Object.const_set(old[:name], old[:constant])
+      end
     end
   end
 
   def define_constant(name, value)
+    begin
+      if old_constant = Object.const_get(name)
+        @old_constants << {:name => name, :constant => old_constant}
+      end
+    rescue NameError
+    end
+
     Object.const_set(name, value)
     @defined_constants << name
   end
