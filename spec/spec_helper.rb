0df7ad1bb2dcd8f394c4358fbdd31dee62f4a881
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-12-11 16:24:13 +1100

    Configured spec helper to load correct dummy app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,25 @@ rescue LoadError
   puts "Skipping SimpleCov"
 end
 
-# require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-
 # require 'rspec/rails'
 require 'delorean'
 require 'json_spec'
 require 'uuid'
 require "crash_log"
 
-# require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+# Super Hax
+gemfile = ENV['BUNDLE_GEMFILE'] + '.lock'
+rails_version = Bundler::LockfileParser.new(File.read(gemfile)).specs.find { |s| s.name == 'rails' }.version.to_s
+
+case rails_version
+when /^3\.(2|1)/
+  require File.expand_path("../dummy_rails_3_plus/config/environment.rb",  __FILE__)
+when /^3\.0\./
+  # Rails 3.0.x
+  require File.expand_path("../dummy_rails_3/config/environment.rb",  __FILE__)
+when /^2/
+  require File.expand_path("../dummy_rails_2/config/environment.rb",  __FILE__)
+end
 
 Dir[File.expand_path("../support/*.rb", __FILE__)].each { |file| require file }
 
