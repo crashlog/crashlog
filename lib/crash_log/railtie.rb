24becd9af06a506f462b4fbf8f1c0d6215e6651e
Ivan Vanderbyl <ivanvanderbyl@me.com>
2012-12-12 15:55:04 +1100

    Reconfigured Railtie to hook into Metal

--- lib/crash_log/railtie.rb
+++ lib/crash_log/railtie.rb
@@ -17,12 +17,12 @@ module CrashLog
 
     config.after_initialize do
       CrashLog.configure(true) do |config|
-        config.params_filters   += Rails.configuration.filter_parameters
-        config.logger           = ::Rails.logger
+        config.params_filters   += ::Rails.application.config.filter_parameters
+        config.logger            = ::Rails.logger
       end
 
       # Attach our Rails Controller methods
-      if defined?(::ActionController::Base)
+      if defined?(::ActionController::Metal)
         require "crash_log/rails/controller_methods"
         ::ActionController::Base.send(:include, CrashLog::Rails::ControllerMethods)
       end
@@ -33,10 +33,11 @@ module CrashLog
         ::ActionDispatch::DebugExceptions.__send__(:include, CrashLog::Rails::Middleware::DebugExceptionCatcher)
       elsif defined?(::ActionDispatch::ShowExceptions)
 
+
         # ActionDispatch::DebugExceptions is not defined in Rails 3.0.x and 3.1.x so
         # catch the exceptions in ShowExceptions.
         require 'crash_log/rails/middleware/debug_exception_catcher'
-        ::ActionDispatch::ShowExceptions.send(:include, CrashLog::Rails::Middleware::DebugExceptionCatcher)
+        ::ActionDispatch::ShowExceptions.__send__(:include, CrashLog::Rails::Middleware::DebugExceptionCatcher)
       end
     end
 
